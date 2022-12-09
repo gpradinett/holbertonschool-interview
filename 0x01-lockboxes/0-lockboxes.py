41c3f0244263e0420f37b4c2ce81f89da8bfc267
gpradinett <gpradinett@gmail.com>
2022-12-08 22:05:02 -0500

    ✅  fix pep 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
 """
 You have n number of locked boxes in front of you.
 Each box is numbered sequentially from 0 to n - 1 and
 each box may contain keys to the other boxes.
 """
+
+
 def canUnlockAll(boxes):
     """ lockboxes app """
     if len(boxes) == 0:
