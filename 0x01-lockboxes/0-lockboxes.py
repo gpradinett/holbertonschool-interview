801639839db81fca07181d37624f18135dab4318
gpradinett <gpradinett@gmail.com>
2022-12-08 22:01:37 -0500

    ✅  n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """ lockboxes app """
+    if len(boxes) == 0:
+        return False
+
+    iBox = [0]
+    for key in iBox:
+        for keyBox in boxes[key]:
+            if keyBox not in iBox:
+                if keyBox < len(boxes):
+                    iBox.append(keyBox)
+    if len(iBox) == len(boxes):
+        return True
+
+    return False
