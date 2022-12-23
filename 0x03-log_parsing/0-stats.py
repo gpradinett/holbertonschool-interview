b7ff4992d558c6aefac345ed5cef27b594ea3f36
gpradinett <gpradinett@gmail.com>
2022-12-22 20:58:23 -0500

    ✅ fix error requirement

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,18 +4,19 @@ script that reads stdin line by line and computes metrics
 """
 import sys
 
+status_codes = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+}
 
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
-
-"""
-Variable to store the total file size
-"""
 total_sizes = 0
 
-"""
-Counter to keep track of the number of lines read
-"""
 line_counter = 1
 
 
@@ -31,9 +32,7 @@ def printStats():
 
 
 try:
-    """ Read lines from stdin one by one """
     for line in sys.stdin:
-        """ Parse the line to extract the status code and file size """
         try:
             line = line[:-1]
             parts = line.split(' ')
@@ -43,16 +42,8 @@ try:
                 status_codes[status_code] += 1
 
         except (ValueError, IndexError):
-            """
-            Skip the line if it is
-            not in the expected format
-            """
             continue
 
-        """
-        Print the statistics every 10 lines
-        or when a keyboard interruption occurs
-        """
         if line_counter % 10 == 0 or line_counter == 0:
             printStats()
         line_counter += 1
