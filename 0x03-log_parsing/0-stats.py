35e41a274821cf82bd21d2dc77bd34cda03fd825
gpradinett <gpradinett@gmail.com>
2022-12-22 20:28:51 -0500

    ✅ script that reads stdin line by line and computes metrics:

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,42 +17,51 @@ status_codes = {
 }
 
 # Variable to store the total file size
-total_size = 0
+total_sizes = 0
 
 # Counter to keep track of the number of lines read
-line_counter = 0
+line_counter = 1
 
-# Read lines from stdin one by one
-for line in sys.stdin:
-    # Parse the line to extract the status code and file size
-    parts = line.split()
-    try:
-        status_code = int(parts[3])
-        file_size = int(parts[4])
-    except (ValueError, IndexError):
-        """
-        Skip the line if it is
-        not in the expected format
-        """
-        continue
 
-    # Update the statistics
-    status_codes[status_code] += 1
-    total_size += file_size
-    line_counter += 1
-
+def printStats():
     """
-    Print the statistics every 10 lines
-    or when a keyboard interruption occurs
+    Print the final statistics after
+    all lines have been read
     """
-    if line_counter % 10 == 0 or line_counter == 0:
-        print("Total file size:", total_size)
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] > 0:
-                print(f"{code}: {status_codes[code]}")
+    print("File size:", total_sizes)
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
 
-# Print the final statistics after all lines have been read
-print("Total file size:", total_size)
-for code in sorted(status_codes.keys()):
-    if status_codes[code] > 0:
-        print(f"{code}: {status_codes[code]}")
+
+try:
+    # Read lines from stdin one by one
+    for line in sys.stdin:
+        # Parse the line to extract the status code and file size
+        try:
+            line = line[:-1]
+            parts = line.split(' ')
+            total_sizes += int(parts[-1])
+            status_code = int(parts[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+        except (ValueError, IndexError):
+            """
+            Skip the line if it is
+            not in the expected format
+            """
+            continue
+
+        """
+        Print the statistics every 10 lines
+        or when a keyboard interruption occurs
+        """
+        if line_counter % 10 == 0 or line_counter == 0:
+            printStats()
+        line_counter += 1
+
+except KeyboardInterrupt:
+    printStats()
+    raise
+printStats()
