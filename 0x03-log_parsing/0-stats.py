3190c8dbc9a620d7ab13301ea1c22bd5f2a8c2ed
gpradinett <gpradinett@gmail.com>
2022-12-22 21:08:15 -0500

    ✅ fix error requirement

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-script that reads stdin line by line and computes metrics
+Write a script that reads stdin line by line and computes metrics
 """
 import sys
 
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
+
+dict_status = {200: 0, 301: 0, 400: 0, 401: 0,
+               403: 0, 404: 0, 405: 0, 500: 0}
 total_sizes = 0
-line_counter = 1
+count_line = 1
 
 
-def printStats():
+def print_stats():
     """
-    Print the final statistics after all lines have been read
+    Prints file size and stats for every 10 loops
     """
-    print("File size:", total_sizes)
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+    print('File size: {}'.format(total_sizes))
+    for code in sorted(dict_status.keys()):
+        if dict_status[code] != 0:
+            print('{}: {}'.format(code, dict_status[code]))
 
 
 try:
@@ -27,17 +28,16 @@ try:
             parts = line.split(' ')
             total_sizes += int(parts[-1])
             status_code = int(parts[-2])
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+            if status_code in dict_status:
+                dict_status[status_code] += 1
+        except Exception:
+            pass
 
-        except (ValueError, IndexError):
-            continue
-
-        if line_counter % 10 == 0 or line_counter == 0:
-            printStats()
-        line_counter += 1
+        if count_line % 10 == 0:
+            print_stats()
+        count_line += 1
 
 except KeyboardInterrupt:
-    printStats()
+    print_stats()
     raise
-printStats()
+print_stats()
