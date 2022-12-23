0dfacf066138c82f4572038938ac6483ca9c8319
gpradinett <gpradinett@gmail.com>
2022-12-22 21:03:07 -0500

    ✅ fix error requirement

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,16 +4,8 @@ script that reads stdin line by line and computes metrics
 """
 import sys
 
-status_codes = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                403: 0, 404: 0, 405: 0, 500: 0}
 
 total_sizes = 0
 line_counter = 1
