2ac56f33d6d5053ec659f375e8c0a62d3ea0ac02
gpradinett <gpradinett@gmail.com>
2023-01-26 03:01:26 -0500

    ✅ upload file

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for i in range(len(coins)):
+        for j in range(coins[i], total + 1):
+            dp[j] = min(dp[j], dp[j - coins[i]] + 1)
+    if dp[total] != float('inf'):
+        return dp[total]
+    else:
+        return -1
