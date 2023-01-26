791ba54616c7a7b9a4e55417f72f926c4d10ad75
gpradinett <gpradinett@gmail.com>
2023-01-26 03:17:04 -0500

    ✅ upload file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,12 +10,12 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    matriz = [float('inf')] * (total + 1)
+    matriz[0] = 0
     for i in range(len(coins)):
         for j in range(coins[i], total + 1):
-            dp[j] = min(dp[j], dp[j - coins[i]] + 1)
-    if dp[total] != float('inf'):
-        return dp[total]
+            matriz[j] = min(matriz[j], matriz[j - coins[i]] + 1)
+    if matriz[total] != float('inf'):
+        return matriz[total]
     else:
         return -1
